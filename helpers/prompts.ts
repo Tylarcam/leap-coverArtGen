cffbc7479ace5352ee921a424fe2d5b1e8c86335
Tylarcam <tylarcam@alumni.stanford.edu>
2023-02-17 14:09:00 -0800

    updated prompt space

--- helpers/prompts.ts
+++ helpers/prompts.ts
@@ -5,28 +5,28 @@ const prompts = [
     prompt:
       "an album cover " +
       keywordIdentifier +
-      "a black and white photo in a city, inspired by Slava Raškaj, harlem renaissance, city high-rise, high rises, high level, 🎨🖌️, idol, ios",
+      " a black and white photo in a city, inspired by Slava Raškaj, harlem renaissance, city high-rise, high rises, high level, 🎨🖌️, idol, ios",
   },
   {
     label: "Abstract",
     prompt:
       "an album cover " +
       keywordIdentifier +
-      "a black and white photo of a cluster of paint splatters, by Howardena Pindell, generative art, abstract, abstract art, abstract painting",
+      " a black and white photo of a cluster of paint splatters, by Howardena Pindell, generative art, abstract, abstract art, abstract painting",
   },
   {
     label: "Conceptual",
     prompt:
       "an album cover " +
       keywordIdentifier +
-      "inspired by Storm Thorgerson, computer art, detailed product image, unknown pleasures, framed poster ",
+      " inspired by Storm Thorgerson, computer art, detailed product image, unknown pleasures, framed poster ",
   },
   {
     label: "Grafitti",
     prompt:
       "an album cover " +
       keywordIdentifier +
-      "standing in front of a graffiti covered wall,  dribble, detalized new york background, artwork in the style of z.w. gu, dripping tar, low res",
+      " standing in front of a graffiti covered wall,  dribble, detalized new york background, artwork in the style of z.w. gu, dripping tar, low res",
   },
 ];
 
